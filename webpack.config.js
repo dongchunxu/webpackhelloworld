6120601b6fe641faa1afeb662230a19e60d7e2e9
dongchunxu <dongchunxu@meituan.com>
2018-08-23 01:53:57 +0800

    r

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,40 @@ var path = require('path')
 
 
 module.exports = { 
-	entry: './src/script/main.js',
+	entry: {
+		main: './src/script/main.js',
+		a: './src/script/a.js',
+		b: './src/script/b.js',
+	},
 	output: {
 		path: path.resolve(__dirname, './dist/'),
-		filename: 'js/[name]-[chunkhash].js'	
+		filename: 'js/[name]-[chunkhash].js',
+		publicPath: 'http://www.haodyinduo2.com/'	
 	},
 	plugins: [
 		new htmlWebpackPlugin({
-			filename: 'index-[hash].html',
+			filename: 'a.html',
 			template: './src/index.html',	//这里为什么不用指定路径
-			inject: 'head',	//script放在head还是body标签里
-		})
+			inject: 'body',	//script放在head还是body标签里
+			minify: true,
+			title: 'this is a ',
+			chunks: ['main', 'a']
+		}),
+		new htmlWebpackPlugin({
+			filename: 'b.html',
+			template: './src/index.html',	//这里为什么不用指定路径
+			inject: 'body',	//script放在head还是body标签里
+			minify: true,
+			title: 'this is b ',
+			chunks: ['main', 'a', 'b']
+		}),
+		new htmlWebpackPlugin({
+			filename: 'c.html',
+			template: './src/index.html',	//这里为什么不用指定路径
+			inject: 'body',	//script放在head还是body标签里
+			minify: true,
+			title: 'this is c ',
+			chunks: ['b']
+		}),
 	]
 };
